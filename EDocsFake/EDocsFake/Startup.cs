4dd26203ee7c75cd07d50a4d98e8b487b63737cc
Luiz Felipe Ribeiro dos Santos <felipeluiz25@hotmail.com>
2020-06-29 20:55:17 -0300

    [Felipe][Auteticação JWT]

--- EDocsFake/EDocsFake/Startup.cs
+++ EDocsFake/EDocsFake/Startup.cs
@@ -1,15 +1,18 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using EDocsFake.Extensions;
+using EDocsFake.Infra.Repository.Interfaces;
+using EDocsFake.Infra.Repository.Repositorios;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace EDocsFake
 {
